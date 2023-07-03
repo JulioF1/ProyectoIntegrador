ef66a8717654fccef41929c165186396fd9f6e35
Sebastian Arnaudo <sebastianarnaudo.lamaruja@gmail.com>
2023-07-03 19:37:23 -0300

    favicon y nav

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
-navbar-light bg-light">
-<div class="container">
-<a class="navbar-brand" href="./index.html">Formulario</a>
-
-</div>
-</nav>`
